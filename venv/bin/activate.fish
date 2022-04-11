e0d78f1253832579629e864f0d5cb517525efd67
anvarinho <anvarinho@gmail.com>
2022-04-11 09:54:09 +0300

    heroku

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/anvarinho/Desktop/PYTHON/Django/venv"
+set -gx VIRTUAL_ENV "/Users/anvarinho/Desktop/PYTHON/Django/guide_API/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
